87b0985dfbc0a2b64a80560b611e24e52d21e0c4
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-07-29 14:40:28 -0300

    SenhaFixa

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Exercício site URI "Senha Fixa"
+// Esse programa iremos digitar uma senha de 4 numeros, caso ela seja invalida mostar "senha invalida" saso seja a senha correta mostrar "Acesso Permitido"
+
+int Senha = int.Parse(Console.ReadLine());
+
+Senha = 0;
+
+while(Senha != 2002)
+{
+    Console.WriteLine("Senha invalida");
+    Senha = int.Parse(Console.ReadLine());
+}
+if (Senha == 2002)
+{
+    Console.WriteLine("Acesso Permitido");
+}
